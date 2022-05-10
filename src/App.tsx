a5ff213316b1bb0b9710ca0e8747867e386d1899
hikornienko <hi.kornienko@gmail.com>
2022-05-10 20:19:25 +0300

    add router page blog:id

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React, { FC } from 'react';
 import { BrowserRouter, HashRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './components/pages/home-page';
+import BlogArticlePage from './components/pages/blog-article-page';
 
 const App: FC = () => (
   <HashRouter>
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/blog/:id" element={<BlogArticlePage />} />
     </Routes>
   </HashRouter>
 );
